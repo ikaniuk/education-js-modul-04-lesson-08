4b0ff6d670b70ec323f047124ac481fba97467ee
Ihor Kaniuk <ikanyuk@icloud.com>
2023-10-09 20:28:08 +0300

    start-lasson

---
+++ js/idex.js
@@ -0,0 +1,29 @@
+// //** Функция с побочными эффектами */
+// const dirtyMultiply = (array, value) => {
+//   for (let i = 0; i < array.length; i += 1) {
+//     array[i] = array[i] * value;
+//   }
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// // Произошла мутация исходных данных - массива numbers
+// console.log(numbers); // [2, 4, 6, 8, 10]
+
+// // Чистая функция (pure function)/
+// const pureMultiply = (array, value) => {
+//   const newArray = [];
+
+//   array.forEach(element => {
+//     newArray.push(element * value);
+//   });
+
+//   return newArray;
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// const doubledNumbers = pureMultiply(numbers, 2);
+
+// // Не произошло мутации исходных данных
+// console.log(numbers); // [1, 2, 3, 4, 5]
+// // Функция вернула новый массив с изменёнными данными
+// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
